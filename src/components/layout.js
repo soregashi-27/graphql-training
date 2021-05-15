42fd03bf00dae3b5d4d4470ad1739ad2f697145b
soregashi-27 <tomo.soul.15@gmail.com>
2021-05-15 15:43:53 +0900

    about.jsの追加

---
+++ src/components/layout.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { css } from "@emotion/react"
+import { link } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+
+export default function Layout({ children }) {
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700xp;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          Pands Eating Lots
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+
+      {children}
+    </div>
+  )
+}
+
+export default layout
