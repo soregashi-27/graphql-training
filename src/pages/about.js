b285aa81f7a4b72b843f856542cca14b2659a392
soregashi-27 <tomo.soul.15@gmail.com>
2021-05-15 18:38:48 +0900

    about pageのタイトルをgraphqlで取得

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
-      <h1>ABOUT Pandas Eating Lots.</h1>
+      <h1>About {data.site.siteMetadata.title}</h1>
       <p>
         We're the only site running on your computer dedicated to showing the
         best photos and videos of pandas eatinf lots of food.
@@ -12,3 +13,13 @@ export default function About() {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
