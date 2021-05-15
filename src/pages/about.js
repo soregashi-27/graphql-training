9c610713285d52b619c6b21f8e1f4d81ed6251f0
soregashi-27 <tomo.soul.15@gmail.com>
2021-05-15 16:00:34 +0900

    about.jsを追加

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>ABOUT Pandas Eating Lots.</h1>
+      <p>
+        We're the only site running on your computer dedicated to showing the
+        best photos and videos of pandas eatinf lots of food.
+      </p>
+    </Layout>
+  )
+}
